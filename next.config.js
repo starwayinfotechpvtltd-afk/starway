e7f68043bc95ec12cf6bb8bbbfd1f814f0bce443
starwayinfotechpvtltd-afk <starwayinfotechpvtltd@gmail.com>
2026-05-14 16:39:22 +0530

    disable turbopack fix

--- next.config.js
+++ next.config.js
@@ -2,18 +2,12 @@
 const nextConfig = {
   poweredByHeader: false,
 
-  async headers() {
-    return [
-      {
-        source: "/_next/static/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
+  images: {
+    unoptimized: true,
+  },
+
+  experimental: {
+    optimizeCss: false,
   },
 };
 
